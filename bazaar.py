c6ddd3be10ce63e842834a2fb9b15237776fd9a4
Puffball101961 <37782411+Puffball101961@users.noreply.github.com>
2022-09-08 23:35:02 +1000

    Updated items.json + parseItem logic

--- bazaar.py
+++ bazaar.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Bazaar:
     def __init__(self):
@@ -9,8 +10,35 @@ class Bazaar:
         self.cachedData = res 
     
     def parseItem(self, item):
-        pass
-    
+        items = open('items.json').read()
+        items = json.loads(items)
+
+        # Check if the item argument is a valid name in items.json
+        for i in range(len(items['items'])):
+            itm = items['items'][i]
+            kl = list(itm.keys())
+            vl = list(itm.values())
+            try:
+                pos = vl.index(item)
+                pid = kl[pos]
+                return pid
+            except ValueError:
+                pass
+
+        # If the item is not an item name, check if it is a productID
+        for i in range(len(items['items'])):
+            itm = items['items'][i]
+            kl = list(itm.keys())
+            vl = list(itm.values())
+            try:
+                pos = kl.index(item)
+                pid = kl[pos]
+                return pid
+            except ValueError:
+                pass
+                
+
+
     def buyOrders(self, item): # Buy Orders for 'item'
         pass
 
@@ -28,4 +56,4 @@ class Bazaar:
 
         
 
-    
+print(Bazaar.parseItem(Bazaar(), 'RED_GIFT'))
