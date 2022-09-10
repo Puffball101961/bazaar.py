ab8551376b513847284c9dd639268ef454d343c0
Puffball101961 <37782411+Puffball101961@users.noreply.github.com>
2022-09-10 11:29:44 +1000

    Update setup

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name='bazaarpy',
     packages=find_packages(include=['bazaarpy']),
-    version='0.1',
+    version='0.1.2',
     description='A Python wrapper for the Hypixel Skyblock Bazaar API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='PuffCode',
     license='GPL-3.0',
     install_requires=['requests'],
     include_package_data=True,
-    package_data={'': ['*.json']}
+    package_data={'': ['*.json']},
+    url='https://github.com/Puffball101961/bazaarpy',
 )
